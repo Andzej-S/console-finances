73c2ff5b83da8eeca24bec2ad8105e3e788a246c
Andzej-S <andrewstreckul@gmail.com>
2022-12-20 20:28:58 +0000

    Add console log code introducing the website and calculating total amount of months

--- index.js
+++ index.js
@@ -86,3 +86,12 @@ var finances = [
 ['Jan-2017', 138230],
 ['Feb-2017', 671099]
 ];
+
+// Console log introduction to what this website is about
+console.log('Financial Analysis');
+console.log('----------------------------');
+
+// Calculation of the  total number of months included in the dataset.
+
+console.log('Total Months: ' + finances.length);
+
